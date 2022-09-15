f97bb8f80ea430a4b7d597d6cd6cac970e03b09f
Veniamin2003 <veniapetrov941@gmail.com>
2022-09-15 08:59:31 +0300

    lesson_51(all)

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
-// import React, { Component } from 'react';
-// import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
-import Dialogs from './components/Dialogs/Dialogs';
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
-
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Settings from "./components/Settings/Settings";
 import DialogsContainer from './components/Dialogs/DialogsContainer';
+import UsersContainer from "./components/Users/UsersContainer";
 
 const App = (props) => {
     debugger;
@@ -25,6 +22,8 @@ const App = (props) => {
                                element={<DialogsContainer />}/>
                         <Route path="/profile"
                                element={<Profile />}/>
+                        <Route path="/users"
+                               element={<UsersContainer />}/>
                         <Route path="/news" element={<News/>}/>
                         <Route path="/music" element={<Music/>}/>
                         <Route path="/Settings" element={<Settings/>}/>
